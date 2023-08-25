9bfe10d71f7a382eef1b86c0309942dc90ed353c
Jonathan-Soto-Official <112433607+Jonathan-Soto-Official@users.noreply.github.com>
2023-08-24 19:54:34 -0500

    Added new file
    Coursera - IBM Front-End Developer Professional Certificate
    Course 1: Introduction to Software Engineering
    Week 2: Tools in Software Engineering
    Activity: Hands-on Lab: Getting Started With an IDE

---
+++ Welcome101.py
@@ -0,0 +1,25 @@
+''' Coursera - IBM Front-End Developer Professional Certificate
+    Course 1: Introduction to Software Engineering
+    Week 2: Tools in Software Engineering
+    Activity: Hands-on Lab: Getting Started With an IDE'''
+
+#Practice Exercises:
+
+#STEP_1: Create a new folder called '''software101'''.
+
+#STEP_2: In it, create a new filed called "software.py".
+
+#STEP_3: Write code to print "software engineering is awesome".
+
+message= "software engineering is awesome"
+print(message)
+
+#STEP_4: Run the program.
+
+#STEP_5: Edit the software.py file and make '''s''' & '''e''' 
+# in '''software engineering is awesome''' to uppercase.
+
+message= "Software Engineering is awesome"
+print(message)
+
+#STEP_6: Run the updated file.
